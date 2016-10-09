39ae92e5af2f4a90a03181b459fc0ae4c96feb35
Eirik Strom <eirikstrom1@gmail.com>
2016-10-09 22:20:35 +0200

    toAsm, compile and main functions added

--- main.hs
+++ main.hs
@@ -1,8 +1,9 @@
 import Data.Char
 import Data.List
 import Data.Maybe
+import System.Environment
 
-data Ast = Number Integer | Name String | App Op [Ast]
+data Ast = Number Integer | Name String | App Op [Ast] | Block [Ast]
     deriving (Show)
 
 data Op = Op
@@ -17,6 +18,12 @@ data ExprElem = Operator Op | Ast Ast
 
 instance Show Op where
     show (Op {symbol=s}) = show s
+
+type AsmLine = String
+type Asm = [AsmLine]
+
+type Register = Integer
+type Registers = [Bool]
 
 operators = [(Op "+" 2 1 0), (Op "-" 2 1 0), (Op "*" 2 2 0), (Op "/" 2 2 0), (Op "++" 1 3 0)]
 extraSymbols = [";", "(", ")"]
@@ -41,10 +48,24 @@ symExists sym = elem sym (opShortList ++ extraSymbols)
 
 --- PARSE
 
+parse :: String -> Ast
+parse str = fst $ parseBlock $ tokenize str
+
+addAst :: Ast -> Ast -> Ast
+addAst (Block list) ast = Block (ast:list)
+
+parseBlock :: [String] -> (Ast, [String])
+parseBlock [] = (Block [], [])
+parseBlock ("}":xs) = (Block [], ("}":xs))
+parseBlock (x:xs) = (addAst (fst block) (fst expr), snd block)
+    where 
+        expr = parseExpr (x:xs)
+        block = parseBlock $ snd expr
+
 parseExpr :: [String] -> (Ast, [String])
 parseExpr [] = (undefined, [])
 parseExpr (x:xs) = (prefixToTree $ infixToPrefix $ fst exprList, drop 1 $ snd exprList)
-        where exprList = getExprList (x:xs)
+    where exprList = getExprList (x:xs)
 
 parseSingleExpr :: [String] -> (Ast, [String])
 parseSingleExpr ("(":xs) = parseExpr xs
@@ -75,7 +96,7 @@ prefixToTreeReq ((Operator op):xs) =
         arg2 = prefixToTreeReq $ snd arg1
 
 infixToPrefix :: [ExprElem] -> [ExprElem]
-infixToPrefix list = reverse $ reverse valueList ++ opList
+infixToPrefix list = reverse opList ++ valueList
     where
         (opList, valueList) = infixToPostfixReq list [] []
 
@@ -100,3 +121,63 @@ getOpFromSym "+" = operators !! 0
 getOpFromSym "-" = operators !! 1
 getOpFromSym "*" = operators !! 2
 getOpFromSym "/" = operators !! 3
+
+--- TO ASM
+
+toAsm :: Ast -> Registers -> (Asm, Register)
+toAsm (Block [x]) regs = (fst $ toAsm x regs, 0)
+toAsm (Block (x:xs)) regs = (expr ++ block, 0)
+    where
+        (expr, _) = toAsm x regs
+        (block, _) = toAsm (Block xs) regs
+
+toAsm (Number x) regs = (["mov " ++ (getRegName reg) ++ ", " ++ (show x)], reg)
+    where
+        reg = regAlloc regs
+
+toAsm (App op exprList) regs
+    | symbol op == "+" = (expr1 ++ expr2 ++ ["add " ++ getRegName reg1 ++ ", " ++ getRegName reg2], reg1)
+    | symbol op == "-" = (expr1 ++ expr2 ++ ["sub " ++ getRegName reg1 ++ ", " ++ getRegName reg2], reg1)
+    | symbol op == "*" = (expr1 ++ expr2 ++ ["mul " ++ getRegName reg1 ++ ", " ++ getRegName reg2], reg1)
+    | symbol op == "/" = (expr1 ++ expr2 ++ ["div " ++ getRegName reg1 ++ ", " ++ getRegName reg2], reg1)
+        where
+            (expr1, reg1) = toAsm (exprList !! 1) regs
+            (expr2, reg2) = toAsm (exprList !! 0) (takeReg regs reg1)
+
+takeReg :: Registers -> Register -> Registers
+takeReg regs reg = take (fromIntegral reg) regs ++ [False] ++ drop (fromIntegral reg + 1) regs
+
+freeReg :: Registers -> Register -> Registers
+freeReg regs reg = take (fromIntegral reg) regs ++ [True] ++ drop (fromIntegral reg + 1) regs
+
+regAlloc :: Registers -> Register
+regAlloc regs =
+    if reg == length regs
+        then error "No more regs"
+        else toInteger reg
+    where
+        reg = length $ takeWhile (== False) regs
+
+emptyRegList :: Registers
+emptyRegList = [True, True, True, True]
+
+getRegName :: Register -> String
+getRegName i = ["rax", "rbx", "rcx", "rdx"] !! fromInteger i
+
+--- COMPILE
+
+compile :: String -> Asm
+compile str = fst $ toAsm (parse str) emptyRegList
+
+--- MAIN
+
+main :: IO ()
+main = do
+    args <- getArgs
+    printAsm $ compile $ head args
+
+printAsm [] = do
+    putStr ""
+printAsm asm = do
+    putStrLn $ head asm
+    printAsm $ tail asm
