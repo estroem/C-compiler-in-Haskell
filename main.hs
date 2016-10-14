b71260dc10d1463cdd1a084662aea073fa1c7789
Eirik Strom <eirikstrom1@gmail.com>
2016-10-14 23:52:43 +0200

    Tokenize updated to include string literals

--- main.hs
+++ main.hs
@@ -61,6 +61,7 @@ tokenize ('/':'/':xs) = tokenize $ dropWhile (/= '\n') xs
 tokenize (x:xs)
     | isDigit x = (x : takeWhile isDigit xs) : tokenize (dropWhile isDigit xs)
     | isAlpha x = (x : takeWhile isAlpha xs) : tokenize (dropWhile isAlpha xs)
+    | x == '"' = (x : takeWhile (/= '"') xs) : tokenize (tail $ dropWhile (/= '"') xs)
     | x == ' ' || x == '\t' || x == '\n' = tokenize xs
     | length xs >= 2 && symExists (x:take 2 xs) = (x : (take 2 xs)) : (tokenize (drop 2 xs))
     | length xs >= 1 && symExists (x:take 1 xs) = (x : (take 1 xs)) : (tokenize (tail xs))
