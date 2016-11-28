41e6b0cf76d6a9fc6a3b4d28730b9ee49a6bdec0
Eirik Strom <eirikstrom1@gmail.com>
2016-11-29 00:09:35 +0100

    Renamed files to start with capital letter and added tokenize

---
+++ src/Tokenize.hs
@@ -0,0 +1,26 @@
+module Tokenize ( tokenize, opShortlist, typeShortlist ) where
+
+import Data.Char
+
+opShortlist = ["+", "-", "*", "/", "++", "=", "$", "==", "!=", "&"]
+
+typeShortlist = ["int", "short", "byte", "char"]
+
+extraSymbols = [";", "(", ")", "{", "}", ",", "[", "]"]
+
+
+tokenize :: String -> [String]
+tokenize [] = []
+tokenize ('/':'/':xs) = tokenize $ dropWhile (/= '\n') xs
+tokenize (x:xs)
+    | isDigit x = (x : takeWhile isDigit xs) : tokenize (dropWhile isDigit xs)
+    | isAlpha x = (x : takeWhile isAlpha xs) : tokenize (dropWhile isAlpha xs)
+    | x == '"' = (x : takeWhile (/= '"') xs) : tokenize (tail $ dropWhile (/= '"') xs)
+    | x == ' ' || x == '\t' || x == '\n' = tokenize xs
+    | length xs >= 2 && symExists (x:take 2 xs) = (x : (take 2 xs)) : (tokenize (drop 2 xs))
+    | length xs >= 1 && symExists (x:take 1 xs) = (x : (take 1 xs)) : (tokenize (tail xs))
+    | symExists [x] = [x] : tokenize xs
+    | otherwise = error $ "Illegal symbol \"" ++ [x] ++ "\""
+
+symExists :: String -> Bool
+symExists sym = elem sym (opShortlist ++ extraSymbols)
